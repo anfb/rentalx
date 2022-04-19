7476f514c53f8a5692a187b66c21a1a3b1fe6fac
Adriely Nara Barbosa <adriely.barbosa@thoughtworks.com>
2022-04-18 21:18:57 -0300

    RX02: Created Category router.

--- src/interface/routers/router.ts
+++ src/interface/routers/router.ts
@@ -1,12 +1,15 @@
 import { HelloWorld } from "@interface/controllers";
+import { CreateCategoriesController } from "@interface/controllers/create-categories-controller";
 import { Express, Router } from "express";
 
 const helloWorldMessage = new HelloWorld()
+const createCategoriesController = new CreateCategoriesController()
 
 export const setRoutes = (app: Express): void => {
     const router = Router()
 
     router.get('/helloWorld', helloWorldMessage.helloWorld.bind(helloWorldMessage))
+    router.post('/categories', createCategoriesController.createCategory.bind(createCategoriesController))
 
     app.use('/rentalx-dev/rentalx', router)
 
